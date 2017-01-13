0c079ee6206a46b3c8b92c54a8de5e3cb11887ed
Jason Rahm <jason.rahm@ucdenver.edu>
2017-01-13 10:53:02 -0700

    routing added for academics pages

--- site-hierarchy/js/app.js
+++ site-hierarchy/js/app.js
@@ -4,34 +4,61 @@ angular.module('conWireframe').run(function($rootScope, $state) {
   $rootScope.$on('$stateChangeStart', function() {
     $rootScope.$state = $state;
   })
+  
 });
 
-//ROUTES
-angular.module('conWireframe').config(function ($stateProvider, $urlRouterProvider) {
-  
-  $stateProvider
-    
-    // View states for main pages linked in Nav
-  
-    .state('home', {
-        url: '/',
-        templateUrl: '../pages/home.html',
-        data: {
-          title: 'CON Wireframe'
-        }
-      });
-  
-//    .state('ledger', {
-//      url: '/',
-//      templateUrl: 'budget/views/ledger.html',
-//      controller: 'ledgerCtrl'
-//    })
-//    .state('report', {
-//      url: '/report',
-//      templateUrl: 'budget/views/report.html',
-//      controller: 'reportCtrl'
-//    });
-  
-  $urlRouterProvider.otherwise('/');
-  
-});
+//
+//angular.module('conWireframe').config(function ($stateProvider, $urlRouterProvider) {
+//  
+//  $stateProvider
+//    
+//    // View states for main pages linked in Nav
+//  
+//    .state('home', {
+//        url: '/',
+//        templateUrl: '/pages/home.html',
+//        data: {
+//          title: 'CON Wireframe'
+//        }
+//      })
+//    .state('academics', {
+//        url: '/academics',
+//        templateUrl: '/pages/academics/academics-landing.html',
+//        data: {
+//          title: 'Academcis'
+//        }
+//      })
+//    .state('undergraduate-programs', {
+//        url: '/academics/undergraduate-programs',
+//        templateUrl: '/pages/academics/program-section.html',
+//        data: {
+//          title: 'Undergraduate Programs'
+//        }
+//      })
+//      .state('graduate-programs', {
+//        url: '/academics/graduate-programs',
+//        templateUrl: '/pages/academics/program-section.html',
+//        data: {
+//          title: 'Graduate Programs'
+//        }
+//      })
+//    .state('graduate-specialties', {
+//        url: '/academics/graduate-specialties',
+//        templateUrl: '/pages/academics/program-section.html',
+//        data: {
+//          title: 'Graduate Specialties'
+//        }
+//      })
+//    .state('additional-programs', {
+//        url: '/academics/additional-programs',
+//        templateUrl: '/pages/academics/program-section.html',
+//        data: {
+//          title: 'Additional Programs'
+//        }
+//      });
+//  
+//  $urlRouterProvider.otherwise('/');
+//  
+//});
+
+
---
+++ site-hierarchy/js/route-files/academics-routes.js
@@ -0,0 +1,38 @@
+angular.module('conWireframe').config(function ($stateProvider, $urlRouterProvider) {
+  
+  $stateProvider
+    
+    // View states for main pages linked in Nav
+  
+    .state('home', {
+      url: '/',
+      templateUrl: '/pages/home.html',
+      data: {
+        title: 'CON Wireframe'
+      }
+    })
+    .state('academics', {
+      url: '/academics',
+      templateUrl: '/pages/academics/academics-landing.html',
+      data: {
+        title: 'Academcis'
+      }
+    })
+    .state('section-landing-page', {
+      url: '/academics/program-section-landing-page',
+      templateUrl: '/pages/academics/program-section.html',
+      data: {
+        title: 'Program Section Landing Page'
+      }
+    })
+    .state('program-landing-page', {
+      url: '/academics/program-landing-page',
+      templateUrl: '/pages/academics/program-landing.html',
+      data: {
+        title: 'Academic Program Landing Page'
+      }
+    });
+  
+  $urlRouterProvider.otherwise('/');
+  
+});
