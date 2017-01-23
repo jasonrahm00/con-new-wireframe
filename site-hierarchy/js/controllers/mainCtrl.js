935147502098f8afae897c18e22d404c46da5448
Jason Rahm <jason.rahm@ucdenver.edu>
2017-01-23 16:20:01 -0700

    made addtions to program landing and academics pages

--- site-hierarchy/js/controllers/mainCtrl.js
+++ site-hierarchy/js/controllers/mainCtrl.js
@@ -2,16 +2,45 @@ angular.module('conWireframe').controller('mainCtrl', function ($scope, programF
   
   'use strict';
   
+  $scope.allPrograms = [];
+  $scope.currentEdLevel;
+  $scope.degreePathSort = [];
+  $scope.pageloadView = 'all';
   $scope.programs;
-  $scope.programLevel = "";
-  $scope.availableCourses = [];
+  $scope.programLevel;
+  
   
   programFactory.getPrograms()
     .then(function(response) {
       $scope.programs = response.data;
+      pushAllPrograms($scope.programs);
     }, function(error) {
       console.log(error.message);
   });
+  
+  function pushAllPrograms(x) {
+    var courses = [];
+    for(var i = 0; i < x.length; i++) {
+      var level = x[i].courses
+      for(var j = 0; j < level.length; j++)
+        $scope.allPrograms.push(level[j]);
+    }
+  }
+  
+//  $scope.edLevelSort = function() {
+//    for(var i = 0; i < $scope.allPrograms.length; i++) {
+//      var program = $scope.allPrograms[i];
+//      for(var j = 0; j < program.audience.length; j++) {
+//        var audience = program.audience[j].toString();
+//        if(audience === $scope.currentEdLevel) {
+//          $scope.degreePathSort.push(program);
+//        } else {
+//          console.log('no audience matches found');
+//        }
+//      }
+//    }
+//    console.log($scope.degreePathSort);
+//  };
   
   jQuery(window).click(function () {
     jQuery('nav>ul>li').removeClass('active');
