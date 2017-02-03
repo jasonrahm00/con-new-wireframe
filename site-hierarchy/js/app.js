a6b47fc9a396bf6c0ca34b15b5bec5b4506b61af
Jason Rahm <jason.rahm@ucdenver.edu>
2017-02-03 16:00:37 -0700

    added conditional class to degree level sort buttons

--- site-hierarchy/js/app.js
+++ site-hierarchy/js/app.js
@@ -1,8 +1,9 @@
-angular.module('conWireframe', ['ui.router']);
+angular.module('conWireframe', ['ui.router', 'angular.filter']);
 
 angular.module('conWireframe').run(function($rootScope, $state) {
   $rootScope.$on('$stateChangeStart', function() {
     $rootScope.$state = $state;
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
   })
   
 });
