aa52913bee8bfd5a75942d56c499d6080b496b1b
Jason Rahm <jason.rahm@ucdenver.edu>
2017-08-01 13:54:46 -0600

    additional about us pages added

--- site-hierarchy/js/route-files/about-routes.js
+++ site-hierarchy/js/route-files/about-routes.js
@@ -11,6 +11,46 @@ angular.module('conWireframe').config(function ($stateProvider, $urlRouterProvid
         title: 'About the CU College of Nursing',
         breadcrumb: 'Home > About'
       }
+    })
+    .state('contact', {
+      url: '/about/contact',
+      templateUrl: '/pages/about/contact.html',
+      data: {
+        title: 'Contact the CU College of Nursing',
+        breadcrumb: 'Home > About > Contact'
+      }
+    })
+    .state('events', {
+      url: '/about/events',
+      templateUrl: '/pages/about/events.html',
+      data: {
+        title: 'Upcoming Events at the CU College of Nursing',
+        breadcrumb: 'Home > About > Events'
+      }
+    })
+    .state('history', {
+      url: '/about/history',
+      templateUrl: '/pages/about/history.html',
+      data: {
+        title: 'History of the CU College of Nursing',
+        breadcrumb: 'Home > About > History'
+      }
+    })
+    .state('meet-dean', {
+      url: '/about/meet-dean',
+      templateUrl: '/pages/about/meet-dean.html',
+      data: {
+        title: 'Meet the CU College of Nursing Dean',
+        breadcrumb: 'Home > About > Meet the Dean'
+      }
+    })
+    .state('news', {
+      url: '/about/news',
+      templateUrl: '/pages/about/news.html',
+      data: {
+        title: 'CU College of Nursing News',
+        breadcrumb: 'Home > About > News'
+      }
     });
   
 });
