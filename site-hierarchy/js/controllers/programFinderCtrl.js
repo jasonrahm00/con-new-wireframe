a9c355ed440f8c4cd9c959bf9537fd526d03aea4
Jason Rahm <jason.rahm@ucdenver.edu>
2017-09-27 12:01:06 -0600

    initial setup for filtering

--- site-hierarchy/js/controllers/programFinderCtrl.js
+++ site-hierarchy/js/controllers/programFinderCtrl.js
@@ -1,5 +1,8 @@
 angular.module('conWireframe').controller('programFinderCtrl', function ($scope, programFactory) {
   
   'use strict';
-    
+  
+  //Faceted Filtering
+    //http://jsfiddle.net/rzgWr/19/
+  
 });
