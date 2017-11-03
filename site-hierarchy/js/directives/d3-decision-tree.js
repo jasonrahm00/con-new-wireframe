5856b6ddf38c472faad8374a64a3312361c32b0c
Jason Rahm <jasonrahm@gmail.com>
2017-11-03 12:09:53 -0600

    refactor code replaced with a different version that adjusts indent when children are opened

--- site-hierarchy/js/directives/d3-decision-tree.js
+++ site-hierarchy/js/directives/d3-decision-tree.js
@@ -1,33 +1,29 @@
 angular.module('conWireframe').directive('decisionTree', function(d3Service){
   
   //indent tree directive refactor source
-    //https://bl.ocks.org/mbostock/1093025
-  
-  //Collapse all nodes on page load
     //http://bl.ocks.org/larskotthoff/7022289
-  
+
   return {
     restrict: 'E',
     scope: {},
-    link: function(scope, element, attrs) {
+    link: function() {
       d3Service.d3().then(function(d3) {
 
-        var margin = {top: 30, right: 20, bottom: 30, left: 20},
-            width = 960 - margin.left - margin.right,
+        var w = 1080,
+            i = 0,
             barHeight = 40,
-            barWidth = width * .8;
-
-        var i = 0,
+            barWidth = w * .75,
             duration = 400,
+            margin = {top: 30, bottom: 30},
             root;
 
         var tree = d3.layout.tree()
-            .nodeSize([0, 40]);
+            .size([0, 40]);
 
-        var svg = d3.select("decision-tree").append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        var vis = d3.select("decision-tree").append("svg:svg")
+            .attr("width", w)
+            .append("svg:g")
+            .attr("transform", "translate(20,30)");
 
         //Collapse all nodes under root on page load
           //https://bl.ocks.org/d3noob/43a860bc0024792f8803bba8ca0d5ecd
@@ -38,13 +34,12 @@ angular.module('conWireframe').directive('decisionTree', function(d3Service){
             node.children = null
           }
         }
-        
-        d3.json("/js/data/tree.json", function(error, data) {
-          if (error) throw error;
-          data.x0 = 0;
-          data.y0 = 0;
-          data.children.forEach(collapse);
-          update(root = data);
+
+        d3.json("/js/data/tree.json", function(json) {
+          json.x0 = 0;
+          json.y0 = 0;
+          json.children.forEach(collapse);
+          update(root = json);
         });
 
         function update(source) {
@@ -53,38 +48,34 @@ angular.module('conWireframe').directive('decisionTree', function(d3Service){
           var nodes = tree.nodes(root);
           
           var height = Math.max(500, nodes.length * barHeight + margin.top + margin.bottom);
-
+          
           d3.select("svg").transition()
               .duration(duration)
               .attr("height", height);
 
-          d3.select(self.frameElement).transition()
-              .duration(duration)
-              .style("height", height + "px");
-
-          // Compute the "layout".
+          // Compute the "layout"
           nodes.forEach(function(n, i) {
             n.x = i * barHeight;
           });
 
           // Update the nodes…
-          var node = svg.selectAll("g.node")
+          var node = vis.selectAll("g.node")
               .data(nodes, function(d) { return d.id || (d.id = ++i); });
 
-          var nodeEnter = node.enter().append("g")
+          var nodeEnter = node.enter().append("svg:g")
               .attr("class", "node")
-              .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
+              .attr("transform", function() { return "translate(" + source.y0 + "," + source.x0 + ")"; })
               .style("opacity", 1e-6);
 
           // Enter any new nodes at the parent's previous position.
-          nodeEnter.append("rect")
+          nodeEnter.append("svg:rect")
               .attr("y", -barHeight / 2)
               .attr("height", barHeight)
               .attr("width", barWidth)
               .style("fill", color)
               .on("click", click);
 
-          nodeEnter.append("text")
+          nodeEnter.append("svg:text")
               .attr("dy", 3.5)
               .attr("dx", 5.5)
               .text(function(d) { return d.name; });
@@ -105,19 +96,16 @@ angular.module('conWireframe').directive('decisionTree', function(d3Service){
           // Transition exiting nodes to the parent's new position.
           node.exit().transition()
               .duration(duration)
-              .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
+              .attr("transform", function() { return "translate(" + source.y + "," + source.x + ")"; })
               .style("opacity", 1e-6)
               .remove();
 
-          // Update the links…
-          var link = svg.selectAll("path.link")
-              .data(tree.links(nodes), function(d) { return d.target.id; });
-          
           // Stash the old positions for transition.
           nodes.forEach(function(d) {
             d.x0 = d.x;
             d.y0 = d.y;
           });
+          
         }
 
         // Toggle children on click.
