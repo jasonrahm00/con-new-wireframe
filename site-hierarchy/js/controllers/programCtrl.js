22dcd909603ce81011595e0234d9ee8ec50e7c9c
Jason Rahm <jason.rahm@ucdenver.edu>
2017-11-21 12:48:54 -0700

    program changer added to degree-landing page

--- site-hierarchy/js/controllers/programCtrl.js
+++ site-hierarchy/js/controllers/programCtrl.js
@@ -4,22 +4,50 @@ angular.module('conWireframe').controller('programCtrl', function ($scope, $q, p
 
   $scope.selectedPathways = [];
   
+  var allPrograms = [];
+  
   programFactory.getPrograms().then(function() {
     var programs = programFactory.programs;
     
     programs.forEach(function(item) {
+      
+      allPrograms.push(item);
+      
       if(item.degree === $scope.chosenDegree) {
         $scope.selectedPathways.push(item)
       }
+      
     });
           
     if($scope.selectedPathways.length === 0) {
-      $scope.selectedPathways = programs;
+      $scope.selectedPathways = allPrograms;
       $scope.degreesFiltered = false;
     } else {
       $scope.degreesFiltered = true;
     }
     
   });
+  
+  $scope.changeDegree = null;
+  
+  $scope.$watch('changeDegree', function(newVal, oldVal) {
+    if(newVal !== oldVal) {
+      
+      $scope.setDegree(newVal.key);
+      $scope.selectedPathways = [];
+      
+      allPrograms.forEach(function(item) {
+        if(item.degree === $scope.chosenDegree) {
+          $scope.selectedPathways.push(item)
+        }
+      });
+      
+      $scope.degreesFiltered = true;
+      
+    }
+  });
+  
+    
+  
   
 });
